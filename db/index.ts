4579201fbe13ae1afe42f3ad14c08258ffb10583
mibrahim2007 <muhammad.jamil76@gmail.com>
2026-05-21 08:20:33 +0500

    Add db connection diagnostics

--- db/index.ts
+++ db/index.ts
@@ -3,6 +3,19 @@ import postgres from 'postgres'
 import { env } from '@/lib/env'
 import * as schema from './schema'
 
-const client = postgres(env.DATABASE_URL, { prepare: false })
+const url = env.DATABASE_URL
+console.log('[db] connecting to:', url.replace(/:[^:@]+@/, ':***@'))
+
+const client = postgres(url, {
+  prepare: false,
+  connect_timeout: 10,
+  onnotice: () => {},
+})
+
+client`SELECT 1`.then(() => {
+  console.log('[db] connection OK')
+}).catch((err: unknown) => {
+  console.error('[db] connection FAILED:', err)
+})
 
 export const db = drizzle(client, { schema })
